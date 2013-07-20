d8012b3101a46bfc473f680db05d489a1895eb9b
Anna Spehar <amisd7@gmail.com>
2013-07-20 14:31:54 +0000

    Corrections to previous

--- web.js
+++ web.js
@@ -1,20 +1,24 @@
+#!/usr/bin/env node
 var express = require('express');
-var app = express();
 var fs = require('fs');
+var infile = "index.html";
+var buffer = Buffer(50); 
+buffer = fs.readFileSync(infile); 
+var outfile = buffer.toString();
 
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-//});
-
-fs.readFile('./index.html', 'utf8', function(err, html) {
-if (err) throw err; 
+app.get('/', function(request, response) {
+response.send('outfile');
 });
 
+//fs.readFile('./index.html', 'utf8', function(err, html) {
+//if (err) throw err; 
+//});
 
 
-var port = process.env.PORT || 5000;
-fs.listen(port, function() {
+
+var port = process.env.PORT || 5000; 
+app.listen(port, function() {
   console.log("Listening on " + port);
 });
