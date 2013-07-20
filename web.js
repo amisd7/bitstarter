e2ced7559316d83dc0bec96ebf7b934512520c4f
Anna Spehar <amisd7@gmail.com>
2013-07-20 14:07:05 +0000

    Changed web.js

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//app.get('/', function(request, response) {
+  //response.send('Hello World 2!');
+//});
+
+fs.readFile('./index.html', 'utf8', function(err, html) {
+if (err) throw err; 
 });
 
+
+
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+fs.listen(port, function() {
   console.log("Listening on " + port);
 });
